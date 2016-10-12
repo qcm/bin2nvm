1f60e86a13d4342245ed86a285f6d702be24ca07
Bo-Yi Lee <boyil@qca.qualcomm.com>
2016-10-11 18:19:16 -0700

    Get the whole bin file length

--- bin2nvm.py
+++ bin2nvm.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 import argparse
+import binascii
 
+NVM_TLV_DATA_START = 4
+NVM_TLV_TAG = 2
+NVM_TLV_LEN = 2
+NVM_TLV_ZERO_PADDING = 8
+NVM_BODY_LEN = 0
+
+bin_data = []
 
 def optParser():
 	parser = argparse.ArgumentParser(description='\tConvert bin file to NVM text')
@@ -8,13 +16,30 @@ def optParser():
 	#parser.print_help()
 	return parser.parse_args()
 
+def fileChecker(inputf, data):
+	if data[0] != b'\x02':
+		print '* ' + inputf + ' is not a valid NVM bin source'
+	else:
+		print '\tStart to converting ' + inputf
+
+def getDataLength(m):
+	print binascii.b2a_hex(m)
+
+# main function
 def bin2nvm():
 	args = optParser()
 	#print args.input_file
 	try:
 		with open(args.input_file, 'rb+') as fobj:
+			bin_data = fobj.read()
+			fileChecker(args.input_file, bin_data)
 			fobj.close()
 	except IOError:
 		print args.input_file + ' is not a valid file name'
+	
+	NVM_BODY_LEN = bin_data[1:NVM_TLV_DATA_START]
+	print NVM_BODY_LEN
+	print binascii.b2a_hex(NVM_BODY_LEN)
+	getDataLength(NVM_BODY_LEN)
 
 bin2nvm()
